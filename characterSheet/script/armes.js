af5dcf6fe4f93a7e3239d22c028dcb9d05184340
Ngrdj <ngrdjhotmail.fr>
2020-04-30 10:01:12 +0200

    Création des scripts persos et armes.

---
+++ characterSheet/script/armes.js
@@ -0,0 +1,21 @@
+const javeline={
+    
+    nom:"Javeline",
+    bonusAtt:"+1",
+    degats:"1d6"
+    
+}
+const baton={
+    
+    nom:"Baton",
+    bonusAtt:"+2",
+    degats:"1d6"
+    
+}
+const dague={
+    
+    nom:"Dague",
+    bonusAtt:"+3",
+    degats:"1d4"
+    
+}
--- characterSheet/script/script.js
+++ characterSheet/script/script.js
@@ -1,83 +1,4 @@
 
-const Namas = {
-    
-    armes : ["1","2","3"],
-    
-    sortPrep : 1,
-    DDSav : "+1",
-    bonusAtt : "+1",
-    
-    force : "+1",
-    dexterite : "+1",
-    constitution : "+1",
-    intelligence : "+1",
-    dexterite : "+1",
-    sagesse : "+1",
-    charisme : "+1"
-    
-    
-    
-}
-const Esus = {
-    
-    sortPrep : 2,
-    DDSav : "+2",
-    bonusAtt : "+2",
-    
-    force : "+2",
-    dexterite : "+2",
-    constitution : "+2",
-    intelligence : "+2",
-    dexterite : "+2",
-    sagesse : "+2",
-    charisme : "+2"
-    
-}
-const Kyra = {
-    
-    sortPrep : 3,
-    DDSav : "+3",
-    bonusAtt : "+3",
-    
-    force : "+3",
-    dexterite : "+3",
-    constitution : "+3",
-    intelligence : "+3",
-    dexterite : "+3",
-    sagesse : "+3",
-    charisme : "+3"
-    
-}
-const Grudu = {
-    
-    sortPrep : 4,
-    DDSav : "+4",
-    bonusAtt : "+4",
-    
-    force : "+4",
-    dexterite : "+4",
-    constitution : "+4",
-    intelligence : "+4",
-    dexterite : "+4",
-    sagesse : "+4",
-    charisme : "+4"
-    
-}
-const Lyre = {
-    
-    sortPrep : 5,
-    DDSav : "+5",
-    bonusAtt : "+5",
-    
-    force : "+5",
-    dexterite : "+5",
-    constitution : "+5",
-    intelligence : "+5",
-    dexterite : "+5",
-    sagesse : "+5",
-    charisme : "+5"
-    
-}
 const playerList = document.getElementById("name");
 
 playerList.addEventListener("change",() => {
@@ -102,6 +23,8 @@ playerList.addEventListener("change",() => {
 })
         
 function loadValues(joueur){
+        
+        const armesTBody=document.getElementById("armesTBody");
     
         const sortPrep = document.getElementById("sortPrep");
         const DDSav = document.getElementById("DDSav");
@@ -124,6 +47,7 @@ function loadValues(joueur){
         intelligence.value = joueur.intelligence;
         sagesse.value = joueur.sagesse;
         charisme.value = joueur.charisme;
+        
         
 }
 playerList.dispatchEvent(new Event('change'));
