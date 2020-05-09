468969afb815709529e915fe3885a773edd6999b
Ngrdj <ngrdjhotmail.fr>
2020-05-09 19:08:13 +0200

    debut script

--- randomStories/script/script.js
+++ randomStories/script/script.js
@@ -1,8 +1,23 @@
 //Fonction Random
 const randomButton=document.getElementById('random');
-let resultRandom=0;
-randomButton.addEventListener('click',()=>{
+const textGenButton = document.getElementsByClassName('textGenButton');
+
+
+/*randomButton.addEventListener('click',()=>{
 	resultRandom=Math.floor((Math.random()*20)+1);
 	console.log(resultRandom);
-})
+})*/
+
 resultRandom=Math.floor((Math.random()*20)+1);
+
+for( const button of textGenButton){
+    
+    button.addEventListener('click', ()=>{
+        
+        let 
+        let RandomNumber = Math.floor((Math.random()*20)+1);
+        
+        
+                            
+    });
+}
