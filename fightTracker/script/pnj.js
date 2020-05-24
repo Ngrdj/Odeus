ac041d9239c85fb442cb0c95f720a3ed2e24717d
Jer1990 <jerome.grand@hotmail.be>
2020-05-24 20:44:47 +0200

    ajout pnj

--- fightTracker/script/pnj.js
+++ fightTracker/script/pnj.js
@@ -4,12 +4,12 @@ const loup = {
     
     classe :'Animal',
     
-    force : "+2",
+    force : "+1",
     dexterite : "+2",
     constitution : "+1",
-    intelligence : "+0",
-    sagesse : "+3",
-    charisme : "-1",
+    intelligence : "-4",
+    sagesse : "+1",
+    charisme : "-2",
     
     details : ""
 
@@ -18,14 +18,142 @@ const gobelin = {
     
     portrait : "https://i1.wp.com/dmdave.com/wp-content/uploads/2020/02/shadow-goblin.png?fit=1500%2C2100&ssl=1",
     
-    classe :'creature',
+    classe :'Monstre',
+    
+    force : "-1",
+    dexterite : "+2",
+    constitution : "+0",
+    intelligence : "+0",
+    sagesse : "-1",
+    charisme : "-1",
+    
+    details : ""
+
+}
+const hobgobelin = {
+    
+    portrait : "https://www.aidedd.org/dnd/images/hobgoblin.jpg",
+    
+    classe :'Monstre',
+    
+    force : "+1",
+    dexterite : "+1",
+    constitution : "+1",
+    intelligence : "+0",
+    sagesse : "+0",
+    charisme : "-1",
+    
+    details : ""
+
+}
+const brigand = {
+    
+    portrait : "https://www.aidedd.org/dnd/images/bandit-captain.jpg",
+    
+    classe :'humanoide',
     
     force : "+2",
     dexterite : "+2",
+    constitution : "+2",
+    intelligence : "-1",
+    sagesse : "+0",
+    charisme : "+1",
+    
+    details : ""
+
+}
+const tavernier = {
+    
+    portrait : "https://i.pinimg.com/originals/d4/eb/bf/d4ebbfccb7f87d0b714d4f6f2fefa46e.jpg",
+    
+    classe :'humanoide',
+    
+    force : "+3",
+    dexterite : "+1",
+    constitution : "+2",
+    intelligence : "-2",
+    sagesse : "-1",
+    charisme : "+0",
+    
+    details : ""
+
+}
+const zombi = {
+    
+    portrait : "https://www.aidedd.org/dnd/images/zombie.jpg",
+    
+    classe :'Monstre',
+    
+    force : "+1",
+    dexterite : "-2",
+    constitution : "+3",
+    intelligence : "-4",
+    sagesse : "-2",
+    charisme : "-3",
+    
+    details : ""
+
+}
+const araignéeG = {
+    
+    portrait : "https://www.aidedd.org/dnd/images/giant-spider.jpg",
+    
+    classe :'Monstre',
+    
+    force : "+2",
+    dexterite : "+3",
     constitution : "+1",
+    intelligence : "-4",
+    sagesse : "+0",
+    charisme : "-3",
+    
+    details : ""
+
+}
+const archer = {
+    
+    portrait : "https://i.pinimg.com/564x/de/ba/50/deba50c800370ce189a016e62925f27e.jpg",
+    
+    classe :'humanoide',
+    
+    force : "+0",
+    dexterite : "+4",
+    constitution : "+3",
     intelligence : "+0",
-    sagesse : "+3",
-    charisme : "-1",
+    sagesse : "+1",
+    charisme : "+0",
+    
+    details : ""
+
+}
+const mageP = {
+    
+    portrait : "https://i.pinimg.com/564x/1d/f2/19/1df21954732a435b9b80d0dcec2cd1bf.jpg",
+    
+    classe :'humanoide',
+    
+    force : "-1",
+    dexterite : "+2",
+    constitution : "+0",
+    intelligence : "+3",
+    sagesse : "+1",
+    charisme : "+0",
+    
+    details : ""
+
+}
+const ogre = {
+    
+    portrait : "https://www.aidedd.org/dnd/images/ogre.jpg",
+    
+    classe :'Monstre',
+    
+    force : "+4",
+    dexterite : "-1",
+    constitution : "+3",
+    intelligence : "-3",
+    sagesse : "-2",
+    charisme : "-2",
     
     details : ""
 
