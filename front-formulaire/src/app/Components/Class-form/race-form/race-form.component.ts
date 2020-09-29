cd1cff62eec2a708cdb1eaac5bf3e373ead0e0dd
jer1990 <jerome.grand@hotmail.be>
2020-09-29 20:08:24 +0200

    formulaire bd

---
+++ front-formulaire/src/app/Components/Class-form/race-form/race-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-race-form',
+  templateUrl: './race-form.component.html',
+  styleUrls: ['./race-form.component.scss']
+})
+export class RaceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
