9b9d04d8da473126429ed6535ae9247cdc3fbfd5
Jer1990 <jerome.grand@hotmail.be>
2020-10-03 15:39:30 +0200

    enum subClass

--- back/src/capacity/capacity.service.ts
+++ back/src/capacity/capacity.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm/dist/common/typeorm.decorators';
+import { Repository } from 'typeorm';
 import { CreateCapacityDto } from './dto/create-capacity.dto';
 import { UpdateCapacityDto } from './dto/update-capacity.dto';
+import { Capacity } from './entities/capacity.entity';
 
 @Injectable()
 export class CapacityService {
+
+  constructor(@InjectRepository(Capacity) private readonly capacityRepository:Repository<Capacity>){}
+
   create(createCapacityDto: CreateCapacityDto) {
     return 'This action adds a new capacity';
   }
--- back/src/sub-class/subClass.enum.ts
+++ back/src/sub-class/subClass.enum.ts
@@ -1,3 +1,18 @@
+import { AfterInsert } from "typeorm";
+
 export enum SubClassEnum{
-    
+    FIRE="FIRE",
+    WATER="WATER",
+    EARTH="EARTH",
+    AIR="AIR",
+    ARCANA="ARCANA",
+    WEATHER="WEATHER",
+    BESTIALITY="BESTIALITY",
+    HERBALIST="HERBALIST",
+    SPIRIT="SPIRIT",
+    DISTANCE="DISTANCE",
+    SURVIVAL="SURVIVAL",
+    FAMILIAR="FAMILIAR",
+    CHAMPION="CHAMPION",
+
 }
