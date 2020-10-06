9cd0c41830c88dc32cf15245a30764b2a1c32551
jer1990 <jerome.grand@hotmail.be>
2020-10-06 21:18:02 +0200

    probleme

--- front-formulaire/src/app/app-routing.module.ts
+++ front-formulaire/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CapacitiesComponent } from './Components/Class-form/capacities/capacities.component';
+import { HomeComponent } from './Views/home/home.component';
 
 
 const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
   {
     path:'newCapacity',
     component:CapacitiesComponent
--- front-formulaire/src/app/app.module.ts
+++ front-formulaire/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapacitiesComponent } from './Components/Class-form/capacities/capacities.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapacitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
